e707e60aa3678f5b0972fa7fca35539663eab23a
ColinTonyDev <colincitoheredia@gmail.com>
2023-03-30 21:40:06 +0000

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -13,13 +13,13 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 ## Left bar
 polybar log -c ~/.config/polybar/current.ini &
 polybar secondary -c ~/.config/polybar/current.ini &
-polybar terciary -c ~/.config/polybar/current.ini &
-polybar quaternary -c ~/.config/polybar/current.ini &
-polybar target -c ~/.config/polybar/current.ini &
+polybar ip_bar -c ~/.config/polybar/current.ini &
+polybar hack_bar -c ~/.config/polybar/current.ini &
 
 ## Right bar
 #polybar top -c ~/.config/polybar/current.ini &
 polybar primary -c ~/.config/polybar/current.ini &
+polybar target_bar -c ~/.config/polybar/current.ini &
 
 ## Center bar
 polybar primary -c ~/.config/polybar/workspace.ini &
